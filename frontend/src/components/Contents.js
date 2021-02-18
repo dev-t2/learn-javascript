d4bee22b28f215be9a8afc13f68df93d241b5dce
t2 <oo_ri@naver.com>
2021-02-19 02:48:58 +0900

    Contents Tailwind CSS Responsive Update

--- frontend/src/components/Contents.js
+++ frontend/src/components/Contents.js
@@ -3,27 +3,29 @@ import { memo } from 'react';
 const Contents = () => {
   return (
     <section>
-      <div className="container lg:mt-4 max-w-7xl mx-auto px-4 flex flex-col lg:flex-row flex-wrap items-center">
-        <div className="flex flex-col w-full justify-center lg:items-start">
-          <h2 className="my-8 text-xl sm:text-2xl font-bold text-blue-800 text-center sm:text-left">
-            모든 화면 사이즈와 플랫폼에서 사용할 수 있는...
-          </h2>
-          <p className="mb-8 text-base sm:text-lg font-semibold text-gray-400 text-center sm:text-left">
-            애플리케이션을 만들려고 노오오오력만 했습니다~!
-          </p>
+      <div className="max-w-7xl px-4 mx-auto mt-8 sm:mt-12 flex flex-col">
+        <h2 className="sm:ml-8 mb-2 sm:mb-4 text-base sm:text-3xl md:text-4xl font-bold text-blue-800 flex justify-center sm:justify-start">
+          THIS, TOO, SHALL PASS AWAY.
+        </h2>
+        <p className="sm:ml-8 mb-8 text-xs sm:text-base md:text-lg font-bold text-gray-400 flex justify-center sm:justify-start">
+          잊지 말자. 지금 내가 열고 들어온 문이 한 때는 다 벽이었다는걸...
+        </p>
 
-          <h2 className="mb-8 font-bold text-pink-500 text-center sm:text-left">
-            Download Application
-          </h2>
+        <h2 className="sm:ml-8 mb-4 text-xs sm:text-base md:text-lg font-bold text-pink-600 flex justify-center sm:justify-start">
+          Download Application
+        </h2>
 
-          <div className="flex w-full justify-center sm:justify-start space-x-4">
-            <img className="h-12 rounded-md" src="images/microsoft.svg" alt="microsoft" />
-            <img className="h-12" src="images/google.svg" alt="google" />
-          </div>
+        <div className="sm:ml-8 mb-10 space-x-4 flex justify-center sm:justify-start">
+          <img
+            className="h-10 sm:h-12 md:h-14 rounded-lg"
+            src="images/microsoft.svg"
+            alt="microsoft"
+          />
+          <img className="h-10 sm:h-12 md:h-14" src="images/google.svg" alt="google" />
         </div>
 
-        <div className="w-full my-6">
-          <img className="mx-auto" src="images/devices.svg" alt="device" />
+        <div className="sm:mr-8 flex justify-center sm:justify-end">
+          <img className="h-36 sm:h-80 md:h-96" src="images/devices.svg" alt="device" />
         </div>
       </div>
     </section>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import Nav from './Nav';
 const Header = ({ title, items }) => {
   return (
     <header className="bg-blue-800">
-      <div className="h-16 max-w-7xl mx-auto px-4 flex items-center justify-between">
+      <div className="h-12 sm:h-16 max-w-7xl mx-auto px-4 flex items-center justify-between">
         <div className="flex-1 flex items-center justify-center sm:justify-start">
           <Logo title={title} />
           <Nav items={items} />
