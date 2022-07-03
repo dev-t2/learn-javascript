92a14320c11c6aa0d77ca76ea39068e18887b0e4
t2 <oo_ri@naver.com>
2022-07-03 20:34:45 +0900

    01-typescript 03-object.ts Update

--- 01-typescript/src/03-object.ts
+++ 01-typescript/src/03-object.ts
@@ -6,22 +6,11 @@ const obj2 = { a: 'a', b: 'b', c: 'c' };
 
 console.log(obj2);
 
-const obj3 = {
-  a: 1,
-  b: 'a',
-  c: true,
-  d: undefined,
-  e: null,
-  f: [1, 2, 3],
-};
+const obj3 = { a: 1, b: 'a', c: true, d: undefined, e: null, f: [1, 2, 3] };
 
 console.log(obj3);
 
-const obj4 = {
-  a: { a1: 1, a2: 2, a3: 3 },
-  b: { b1: 4, b2: 5, b3: 6 },
-  c: { c1: 7, c2: 8, c3: 9 },
-};
+const obj4 = { a: { a1: 1, a2: 2 }, b: { b1: 4, b2: 5 }, c: { c1: 7, c2: 8 } };
 
 console.log(obj4);
 console.log(obj4.a);
