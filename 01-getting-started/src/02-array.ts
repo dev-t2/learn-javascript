4cfed4b5fc411d5231da4c2ced723c487a0bef59
t2 <oo_ri@naver.com>
2022-07-03 20:04:11 +0900

    01-getting-started 02-array.ts Update

---
+++ 01-getting-started/src/02-array.ts
@@ -0,0 +1,16 @@
+const arr1 = [1, 2, 3, 4, 5];
+
+console.log(arr1);
+
+const arr2 = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr2);
+
+const arr3 = [1, 'a', true, undefined, null];
+
+console.log(arr3);
+console.log(arr3[0]);
+console.log(arr3[1]);
+console.log(arr3[2]);
+console.log(arr3[3]);
+console.log(arr3[4]);
