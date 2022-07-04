cc5831d4ef3d4d3f58b0b1523627563edf579447
t2 <oo_ri@naver.com>
2022-07-04 10:58:58 +0900

    02-tsx Book.tsx Update

--- 02-tsx/src/App.tsx
+++ 02-tsx/src/App.tsx
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { memo } from 'react';
+import Book from './components/Book';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Book name="JavaScript" numOfPage={300} />
+      <Book name="TypeScript" numOfPage={200} />
+      <Book name="React" numOfPage={400} />
     </div>
   );
-}
+};
 
-export default App;
+export default memo(App);
