1365a6d1a2f06e613d4158476934a1905631e31e
dev-t2 <oo_ri@naver.com>
2023-02-26 17:06:51 +0900

    1. Data > 02. Number Update

---
+++ src/1-data/02-number.js
@@ -0,0 +1,21 @@
+const number1 = 10;
+const number2 = -10;
+
+console.log(number1 + number2);
+
+const number3 = 3.14;
+
+console.log({ number3 });
+
+const number4 = 10 + undefined;
+
+console.log({ number4 });
+console.log(typeof number4);
+
+const number5 = 0.1 + 0.2;
+
+console.log({ number5 });
+console.log(number5.toFixed(1));
+console.log(typeof number5.toFixed(1));
+console.log(Number(number5.toFixed(1)));
+console.log(typeof Number(number5.toFixed(1)));
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+/* 1. JavaScript Data Type */
 // import './1-data/01-string';
 import './1-data/02-number';
