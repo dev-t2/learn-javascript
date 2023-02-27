3e6e8f27fd7b209e2fb8ea2737deb8c791cec335
dev-t2 <oo_ri@naver.com>
2023-02-27 23:53:24 +0900

    1. Data > 06. Array Update

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@
 // import './1-data/02-number';
 // import './1-data/03-boolean';
 // import './1-data/04-null';
-import './1-data/05-undefined';
+// import './1-data/05-undefined';
+import './1-data/06-array.js';
