82a689f93b4b96db2349a61ab9d6c3000ca61ed8
dev-t2 <oo_ri@naver.com>
2023-03-01 16:50:09 +0900

    1. Data > 03. Boolean Update

--- src/1-data/03-boolean.js
+++ src/1-data/03-boolean.js
@@ -1,10 +1,55 @@
-const boolean1 = true;
-const boolean2 = false;
-
-if (boolean1) {
-  console.log('True');
+if (true) {
+  console.log(true);
 }
 
-if (boolean2) {
-  console.log('False');
+if (false) {
+  console.log(false);
+}
+
+if (123) {
+  console.log(123);
+}
+
+if (0) {
+  console.log(0);
+}
+
+if (-0) {
+  console.log(-0);
+}
+
+if (0n) {
+  console.log(-0);
+}
+
+if ('0') {
+  console.log('0');
+}
+
+if ('') {
+  console.log('');
+}
+
+if (null) {
+  console.log(null);
+}
+
+if (undefined) {
+  console.log(undefined);
+}
+
+if (NaN) {
+  console.log(undefined);
+}
+
+const a = [1, 2];
+
+if (a) {
+  console.log({ a });
+}
+
+const b = [];
+
+if (b.length) {
+  console.log({ b });
 }
