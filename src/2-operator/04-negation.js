ec5297656c666c02147211b61335660f17f3fe9c
dev-t2 <oo_ri@naver.com>
2023-03-04 23:30:14 +0900

    2. Operator > 04. Negation Update

---
+++ src/2-operator/04-negation.js
@@ -0,0 +1,15 @@
+console.log(!true);
+console.log(!false);
+
+console.log(0);
+console.log(!0);
+console.log(!!0);
+console.log(!!!0);
+
+console.log(!null);
+console.log(!undefined);
+console.log(!NaN);
+console.log(!'');
+
+console.log(!{});
+console.log(![]);
