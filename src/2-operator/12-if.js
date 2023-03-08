cb8be77169dee8099bcc2ad66ef636e25ba084bb
dev-t2 <oo_ri@naver.com>
2023-03-09 00:04:02 +0900

    2. Operator > 12. If Update

---
+++ src/2-operator/12-if.js
@@ -0,0 +1,13 @@
+const isPositive = (number) => {
+  if (number > 0) {
+    return 'Positive Number';
+  } else if (number < 0) {
+    return 'Negative Number';
+  } else {
+    return `${number}`;
+  }
+};
+
+console.log(isPositive(1));
+console.log(isPositive(-1));
+console.log(isPositive(0));
