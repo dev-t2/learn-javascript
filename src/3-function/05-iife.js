31088a438e6c3c84fc8b2a8ffa54c4697a25c839
dev-t2 <oo_ri@naver.com>
2023-03-18 23:33:15 +0900

    3. Function > 05. IIFE Update

---
+++ src/3-function/05-iife.js
@@ -0,0 +1,20 @@
+// IIFE: Immediately Invoked Function Expression
+
+const a = 4;
+
+const func1 = () => {
+  console.log(a * 2);
+};
+
+func1();
+
+(() => {
+  console.log(a * 3);
+})();
+
+console.log('');
+
+((w, d) => {
+  console.log(w.innerWidth);
+  console.log(d.body);
+})(window, document);
