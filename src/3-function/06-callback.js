ba4c35bd265bd36bacd138800861aa41cd84b832
dev-t2 <oo_ri@naver.com>
2023-03-19 00:11:18 +0900

    3. Function > 06. Callback Update

---
+++ src/3-function/06-callback.js
@@ -0,0 +1,46 @@
+const func1 = (callback) => {
+  console.log('func1');
+
+  callback();
+};
+
+const func2 = () => {
+  console.log('func2');
+};
+
+func1(func2);
+
+const func3 = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000);
+};
+
+func3(1, 2, (result) => {
+  console.log(result);
+});
+
+const loadImage = (url, callback) => {
+  const imgElement = document.createElement('img');
+
+  imgElement.src = url;
+
+  imgElement.addEventListener('load', () => {
+    setTimeout(() => {
+      callback(imgElement);
+    }, 1000);
+  });
+};
+
+const bodyElement = document.querySelector('body');
+
+bodyElement.innerHTML = 'Loading...';
+
+loadImage(
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433',
+  (imgElement) => {
+    bodyElement.innerHTML = '';
+
+    bodyElement.appendChild(imgElement);
+  }
+);
