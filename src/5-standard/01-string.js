6d0c7dcab6acdbd88ee65e7426c69953c2aca0f5
dev-t2 <oo_ri@naver.com>
2023-03-19 23:52:58 +0900

    5. Standard > 01. String Update

--- src/5-standard/01-string.js
+++ src/5-standard/01-string.js
@@ -1,3 +1,26 @@
 const a = 'Hello World';
 
-console.log(a.length);
+console.log(`length: ${a.length}`);
+
+console.log('');
+
+console.log(`includes: ${a.includes('Hello')}`);
+console.log(`includes: ${a.includes('hello')}`);
+console.log(`includes: ${a.includes('Hello', 1)}`);
+
+console.log('');
+
+console.log(`indexOf: ${a.indexOf('World')}`);
+console.log(`indexOf: ${a.indexOf('world')}`);
+
+console.log('');
+
+const b = '123456';
+
+console.log(`padStart: ${b.padStart(10, '0')}`);
+console.log(`padStart: ${b.padStart(5, '0')}`);
+
+console.log('');
+
+console.log(`padEnd: ${b.padEnd(10, '0')}`);
+console.log(`padEnd: ${b.padEnd(5, '0')}`);
