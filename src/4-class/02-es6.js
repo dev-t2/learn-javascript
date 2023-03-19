9480d9c4be101a86810679f15c2e41c65e15fefe
dev-t2 <oo_ri@naver.com>
2023-03-19 16:41:01 +0900

    4. Class > 02. ES6 Update

--- src/4-class/02-es6.js
+++ src/4-class/02-es6.js
@@ -6,9 +6,27 @@ class User {
   getName() {
     return this.name;
   }
+
+  set customName(name) {
+    console.log('Setter Name');
+
+    this.name = name;
+  }
+
+  get customName() {
+    console.log('Getter Name');
+
+    return this.name;
+  }
 }
 
 const userA = new User('User A');
 
 console.log(userA);
 console.log(userA.getName());
+
+console.log('');
+
+userA.customName = 'User B';
+
+console.log(userA.customName);
