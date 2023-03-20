17f4ae086f775dc956169b577681cb7c9c5a48d8
dev-t2 <oo_ri@naver.com>
2023-03-20 23:55:10 +0900

    5. Standard > 01. String Update

--- src/5-standard/01-string.js
+++ src/5-standard/01-string.js
@@ -20,7 +20,39 @@ const b = '123456';
 console.log(`padStart: ${b.padStart(10, '0')}`);
 console.log(`padStart: ${b.padStart(5, '0')}`);
 
-console.log('');
-
 console.log(`padEnd: ${b.padEnd(10, '0')}`);
 console.log(`padEnd: ${b.padEnd(5, '0')}`);
+
+console.log('');
+
+const c = 'Hello, Hello?!';
+
+console.log(`replace: ${c.replace('Hello', 'Hi')}`);
+console.log(`replace: ${c.replace(/Hello/g, 'Hi')}`);
+
+console.log('');
+
+const d = 'Hello World!';
+
+console.log(`slice: ${d.slice(0, 5)}`);
+console.log(`slice: ${d.slice(6, -1)}`);
+console.log(`slice: ${d.slice(6)}`);
+
+console.log('');
+
+const e = 'Apple, Banana, Cherry';
+
+console.log(e.split(','));
+console.log(e.split(', '));
+console.log(e.split('').reverse().join(''));
+
+console.log('');
+
+console.log(`toLowerCase: ${e.toLowerCase()}`);
+console.log(`toUpperCase: ${e.toUpperCase()}`);
+
+console.log('');
+
+const f = '     Java   Script     ';
+
+console.log(`trim: ${f.trim()}`);
