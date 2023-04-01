44543400410ad545088dda1faf9da273c4c219e9
dev-t2 <oo_ri@naver.com>
2023-04-01 20:49:29 +0900

    5. Standard > 05. Array #4 Update

--- src/5-standard/05-array.js
+++ src/5-standard/05-array.js
@@ -118,3 +118,98 @@ console.log(`push: ${array5.push(3)}`);
 console.log(array5);
 console.log(`push: ${array5.push(4, 5)}`);
 console.log(array5);
+
+console.log('');
+
+console.log(array5.reverse());
+console.log(array5);
+
+console.log('');
+
+console.log(`shift: ${array5.shift()}`);
+console.log(array5);
+
+console.log('');
+
+const array6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(array6.slice(0, 3));
+console.log(array6.slice(4, -1));
+console.log(array6.slice(4));
+
+console.log('');
+
+console.log(`some: ${array6.some((arr) => arr > 5)}`);
+console.log(`some: ${array6.some((arr) => arr > 9)}`);
+
+console.log('');
+
+const array7 = [4, 8, 1, 100, 0, 16];
+
+console.log(array7);
+
+console.log('');
+
+console.log(array7.sort());
+console.log(array7);
+
+console.log('');
+
+console.log(array7.sort((a, b) => a - b));
+console.log(array7);
+
+console.log('');
+
+console.log(array7.sort((a, b) => b - a));
+console.log(array7);
+
+console.log('');
+
+const array8 = [1, 2, 3];
+
+console.log(array8);
+
+console.log('');
+
+console.log(array8.splice(2, 0, 4));
+console.log(array8);
+
+console.log('');
+
+console.log(array8.splice(1, 2, 5, 6));
+console.log(array8);
+
+console.log('');
+
+const array9 = [1, 2, 3];
+
+console.log(`unshift: ${array9.unshift(4)}`);
+console.log(array9);
+
+console.log('');
+
+const arrayLike1 = {
+  0: 0,
+  1: 1,
+  2: 2,
+  length: 3,
+};
+
+const arrayLike2 = {
+  0: 0,
+  1: 1,
+  2: 2,
+};
+
+console.log(`isArray: ${arrayLike1.constructor === Array}`);
+console.log(`isObject: ${arrayLike1.constructor === Object}`);
+
+Array.from(arrayLike1).forEach((arr) => console.log(`arrayLike1: ${arr}`));
+Array.from(arrayLike2).forEach((arr) => console.log(`arrayLike2: ${arr}`));
+
+console.log('');
+
+const array10 = [1, 2, 3];
+
+console.log(`isArray: ${Array.isArray(array10)}`);
+console.log(`isArray: ${Array.isArray(arrayLike1)}`);
