21230eb64b0266a5dd8f7779e96679f255e6b934
dev-t2 <oo_ri@naver.com>
2023-04-01 17:20:59 +0900

    5. Standard > 05. Array #2 Update

--- src/5-standard/05-array.js
+++ src/5-standard/05-array.js
@@ -30,4 +30,48 @@ const users = [
 
 const adults = users.filter((user) => user.age >= 19);
 
-console.log({ adults });
+console.log(adults);
+
+console.log('');
+
+const findUser = users.find((user) => user.age >= 19);
+
+console.log(findUser);
+
+const index = users.findIndex((user) => user.age >= 19);
+
+console.log(index);
+
+console.log('');
+
+const array3 = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+
+console.log(array3.flat());
+console.log(array3.flat(2));
+console.log(array3.flat(Infinity));
+
+const array4 = [1, 2, 3];
+
+console.log('');
+
+array4.forEach((arr) => console.log(arr));
+
+console.log('');
+
+for (let i = 0; i < array4.length; i += 1) {
+  if (i > 1) {
+    break;
+  }
+
+  console.log(array4[i]);
+}
+
+console.log('');
+
+console.log(`includes: ${array4.includes(2)}`);
+console.log(`includes: ${array4.includes(4)}`);
+
+console.log('');
+
+console.log(`includes: ${users.includes({ id: 3, name: 'User C', age: 35 })}`);
+console.log(`includes: ${users.includes(users[2])}`);
