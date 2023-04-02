178b451ea3b643f7fbb201b88bd7687dedba2492
dev-t2 <oo_ri@naver.com>
2023-04-02 22:58:29 +0900

    1. Data > 07. Object Code Refactoring

--- src/1-data/07-object.js
+++ src/1-data/07-object.js
@@ -1,7 +1,8 @@
-const object1 = new Object();
-object1.name = 'User A';
+const obj1 = new Object();
 
-console.log(object1);
+obj1.name = 'User A';
+
+console.log(obj1);
 
 console.log('');
 
@@ -9,41 +10,39 @@ function Object2() {
   this.name = 'User B';
 }
 
-const object2 = new Object2();
+const obj2 = new Object2();
 
-console.log(object2);
+console.log(obj2);
 
 console.log('');
 
-const object3 = { id: 1, name: 'User C', age: 100, age: 35 };
+const obj3 = { id: 1, name: 'User C', age: 100, age: 35 };
 
-console.log(object3);
-console.log(object3.name);
-console.log(object3['name']);
+console.log(obj3);
+console.log(`obj3.name: ${obj3.name}`);
+console.log(`obj3['name']: ${obj3['name']}`);
 
 const key = 'name';
 
-console.log(object3[key]);
+console.log(`key: ${key}`);
+console.log(`obj3[key]: ${obj3[key]}`);
 
 console.log('');
 
-const object4 = { id: 1, name: 'User D', age: 33, parent: object3 };
+const obj4 = { id: 2, name: 'User D', age: 16, parent: obj3 };
 
-console.log(object4);
-console.log(object4.parent);
-
-console.log('');
-
-console.log(object4.parent.name);
-console.log(object4.parent['name']);
-console.log(object4['parent'].name);
-console.log(object4['parent']['name']);
+console.log(obj4);
+console.log(obj4.parent);
+console.log(`obj4.parent.name: ${obj4.parent.name}`);
+console.log(`obj4.parent['name']: ${obj4.parent['name']}`);
+console.log(`obj4['parent'].name: ${obj4['parent'].name}`);
+console.log(`obj4['parent']['name']: ${obj4['parent']['name']}`);
 
 console.log('');
 
-const objects = [object3, object4];
+const objects = [obj3, obj4];
 
 console.log(objects);
 console.log(objects[0]);
-console.log(objects[0].name);
-console.log(objects[0]['name']);
+console.log(`objects[0].name: ${objects[0].name}`);
+console.log(`objects[0]['name']: ${objects[0]['name']}`);
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
 // import './1-data/03-boolean';
 // import './1-data/04-null';
 // import './1-data/05-undefined';
-import './1-data/06-array';
-// import './1-data/07-object';
+// import './1-data/06-array';
+import './1-data/07-object';
 // import './1-data/08-function';
 // import './1-data/09-conversion';
 // import './1-data/10-type';
