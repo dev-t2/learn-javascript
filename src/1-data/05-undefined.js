f08fd84685baacdd7860558879988795654f4285
dev-t2 <oo_ri@naver.com>
2023-04-02 22:49:33 +0900

    1. Data > 05. Undefined Code Refactoring

--- src/1-data/05-undefined.js
+++ src/1-data/05-undefined.js
@@ -1,13 +1,12 @@
 const a = undefined;
+
 let b;
 
-console.log({ a, b });
-console.log(typeof a);
-console.log(typeof b);
+console.log(`a: ${a}, b: ${b}`);
+console.log(`typeof a: ${typeof a}, typeof b: ${typeof b}`);
 
 console.log('');
 
 const user = { id: 1, name: 'User A' };
 
-console.log(user.name);
-console.log(user.email);
+console.log(`user.name: ${user.name}, user.email: ${user.email}`);
