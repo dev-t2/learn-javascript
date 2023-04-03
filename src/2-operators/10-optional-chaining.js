e33e26738ee1a7cd2477d0c9efd0ace7445f11ea
dev-t2 <oo_ri@naver.com>
2023-04-03 23:56:22 +0900

    2. Operators > 10. Optional Chaining Code Refactoring

--- src/2-operators/10-optional-chaining.js
+++ src/2-operators/10-optional-chaining.js
@@ -2,13 +2,11 @@ const user1 = {
   name: 'User A',
 };
 
-console.log(user1.name);
-
-console.log('');
+console.log(`user1.name: ${user1.name}`);
 
 const user2 = null;
 
-console.log(user2?.name);
+console.log(`user2?.name: ${user2?.name}`);
 
 console.log('');
 
@@ -16,5 +14,5 @@ const getName = (user) => {
   return user?.name || 'Not Found';
 };
 
-console.log(getName(user1));
-console.log(getName(user2));
+console.log(`getName(user1): ${getName(user1)}`);
+console.log(`getName(user2): ${getName(user2)}`);
