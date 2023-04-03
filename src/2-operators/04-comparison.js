dd767ed7d78143064ac67e19b53ac4d892a25969
dev-t2 <oo_ri@naver.com>
2023-04-03 23:38:58 +0900

    2. Operators > 04. Comparison Code Refactoring

--- src/2-operators/04-comparison.js
+++ src/2-operators/04-comparison.js
@@ -1,20 +1,17 @@
 const a = 4;
 const b = '4';
 
-console.log(a == b);
-console.log(a != b);
-
-console.log('');
-
-console.log(a === b);
-console.log(a !== b);
+console.log(`a == b: ${a == b}`);
+console.log(`a != b: ${a != b}`);
+console.log(`a === b: ${a === b}`);
+console.log(`a !== b: ${a !== b}`);
 
 console.log('');
 
 const c = 4;
 const d = 5;
 
-console.log(c > d);
-console.log(c >= d);
-console.log(c < d);
-console.log(c <= d);
+console.log(`c > d: ${c > d}`);
+console.log(`c >= d: ${c >= d}`);
+console.log(`c < d: ${c < d}`);
+console.log(`c <= d: ${c <= d}`);
