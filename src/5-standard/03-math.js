536f4a534f7f21f99c59c5c20c0761b0f6dfe425
dev-t2 <oo_ri@naver.com>
2023-04-19 23:40:16 +0900

    5. Standard > 03. Math Code Refactoring

--- src/5-standard/03-math.js
+++ src/5-standard/03-math.js
@@ -1,26 +1,26 @@
-console.log(`abs: ${Math.abs(4)}`);
-console.log(`abs: ${Math.abs(-4)}`);
+console.log(`Math.abs(4): ${Math.abs(4)}`);
+console.log(`Math.abs(-4): ${Math.abs(-4)}`);
 
 console.log('');
 
-console.log(`ceil: ${Math.ceil(3.1415926535)}`);
-console.log(`round: ${Math.round(3.1415926535)}`);
-console.log(`round: ${Math.round(4.56)}`);
-console.log(`floor: ${Math.floor(3.1415926535)}`);
+console.log(`Math.ceil(3.1415926535): ${Math.ceil(3.1415926535)}`);
+console.log(`Math.round(3.1415926535): ${Math.round(3.1415926535)}`);
+console.log(`Math.round(4.56): ${Math.round(4.56)}`);
+console.log(`Math.floor(3.1415926535): ${Math.floor(3.1415926535)}`);
 
 console.log('');
 
-console.log(`max: ${Math.max(4, 8, 16, 1000, 32)}`);
-console.log(`min: ${Math.min(4, 8, 16, -10, 32)}`);
+console.log(`Math.max(4, 8, 16, 1000, 32): ${Math.max(4, 8, 16, 1000, 32)}`);
+console.log(`Math.min(4, 8, 16, -10, 32): ${Math.min(4, 8, 16, -10, 32)}`);
 
 console.log('');
 
-console.log(`pow: ${Math.pow(4, 2)}`);
-console.log(`pow: ${Math.pow(10, 3)}`);
+console.log(`Math.pow(4, 2): ${Math.pow(4, 2)}`);
+console.log(`Math.pow(10, 3): ${Math.pow(10, 3)}`);
 
 console.log('');
 
-console.log(`random: ${Math.random()}`);
+console.log(`Math.random(): ${Math.random()}`);
 
 console.log('');
 
@@ -28,5 +28,5 @@ const getRandom = (min = 0, max = 1) => {
   return Math.random() * (max - min) + min;
 };
 
-console.log(`getRandom: ${getRandom()}`);
-console.log(`getRandom: ${getRandom(10, 20)}`);
+console.log(`getRandom(): ${getRandom()}`);
+console.log(`getRandom(10, 20): ${getRandom(10, 20)}`);
