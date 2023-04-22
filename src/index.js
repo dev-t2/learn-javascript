60b75206379f4c2d305caef6c3a2d0a5b75972f9
dev-t2 <oo_ri@naver.com>
2023-04-22 15:41:40 +0900

    Learn JavaScript Code Refactoring Complete

--- src/index.js
+++ src/index.js
@@ -54,5 +54,5 @@
 // import './6-asynchronous/02-error';
 // import './6-asynchronous/03-promise';
 // import './6-asynchronous/04-reject';
-import './6-asynchronous/05-async-await';
-// import './6-asynchronous/06-try-catch';
+// import './6-asynchronous/05-async-await';
+import './6-asynchronous/06-try-catch';
